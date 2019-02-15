89ca57162585c53fe9f9927cef184a0d8c46e5cc
Agnes NM <agnesndutamwaniki@gmail.com>
2019-02-15 17:44:58 +0300

    add paragraph elements to the form

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-function checkNow () {
-  var length = document.getElementById('length').value;
-}
+function Submit () {
+  var triangleTracker1 = document.getElementById('triangleTracker1').innerHTML;
+  var triangleTracker2 = document.getElementById('triangleTracker2').innerHTML;
+  var triangleTracker3 = document.getElementById('triangleTracker3').innerHTML;
+  if (triangleTracker1 === triangleTracker2 === triangleTracker3) {
+    alert ("Equilateral triangle");
+  }
