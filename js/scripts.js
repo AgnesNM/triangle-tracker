2e132e651ef5662baf6ee589a06893c52f503a49
Agnes NM <agnesndutamwaniki@gmail.com>
2019-02-16 08:01:12 +0300

    change scalene business logic

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ var sides = [];
     alert ("isosceles Triangle");
   } else if (side1 !== side2 && side2 !== side3 && side1 !== side3 || side1 + side2 > side3 || side2 + side3 > side1 || side3 + side1 > side2) {
     alert ("Scalene Triangle");
-  } else {
-    alert("Not a Triangle")
+  } else if (side1 + side2 <= side3 || side2 + side3 <= side1) {
+    alert("Not a Triangle");
   }
 }
