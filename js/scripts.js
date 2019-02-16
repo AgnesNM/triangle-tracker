44d61e92e995e21dd8ecf249d4cad6cf793c921f
Agnes NM <agnesndutamwaniki@gmail.com>
2019-02-16 08:10:28 +0300

    change not a triangle business logic

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,9 @@ var sides = [];
     alert ("Equilateral Triangle");
   } else if (side1 === side2 || side2 === side3 || side1 === side3){
     alert ("isosceles Triangle");
-  } else if (side1 !== side2 && side2 !== side3 && side1 !== side3 || side1 + side2 > side3 || side2 + side3 > side1 || side3 + side1 > side2) {
+  } else if (side1 !== side2 && side2 !== side3 && side3 !== side1 && side1 + side2 > side3 && side2 + side3 > side1 && side3 + side1 > side2) {
     alert ("Scalene Triangle");
-  } else if (side1 + side2 <= side3 || side2 + side3 <= side1) {
+  } else if (side1 + side2 <= side3 || side2 + side3 <= side1 || side3 + side1 <= side2) {
     alert("Not a Triangle");
   }
 }
