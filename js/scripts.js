10222f75d6b41819cf34c7b00b84d4b788cb2205
Agnes NM <agnesndutamwaniki@gmail.com>
2019-02-17 06:55:46 +0300

    change div border to yellow

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
-function triangleSides() {
-  var side1 = document.getElementById('side1').value;
-  var side2 = document.getElementById('side2').value;
-  var side3 = document.getElementById('side3').value;
-  var sides = [];
+function triangleSides () {
+var side1 = document.getElementById('side1').value;
+var side2 = document.getElementById('side2').value;
+var side3 = document.getElementById('side3').value;
+var sides = [];
   sides.push(side1, side2, side3);
   parseInt(sides);
-  if (side1 === side2 && side2 === side3 && side3 === side1) {
-    alert("Equilateral Triangle");
-  } else if (side1 === side2 || side2 === side3 || side1 === side3) {
-    alert("Isosceles Triangle");
-  } else if (side1 !== side2 && side2 !== side3 && side3 !== side1 || (side1 + side2) > side3 || (side2 + side3) > side1 || (side3 + side1) > side2) {
-    alert("Scalene Triangle");
-  } else if (side1 + side2) <= side3 || (side2 + side3) <= side1 || (side3 + side2) <= side1) {
-  alert("Not a Triangle");
-}
+  if (side1 === side2 && side2 === side3 && side1 === side3) {
+    alert ("Equilateral Triangle");
+  } else if (side1 === side2 || side2 === side3 || side1 === side3){
+    alert ("isosceles Triangle");
+  } else if (side1 !== side2 && side2 !== side3 && side1 !== side3 && (side1 + side2 > side3 && side2 + side3 > side1) || side3 + side1 > side2) {
+    alert ("Scalene Triangle");
+  } else if ((side1 + side2 <= side3 && side2 + side3 <= side1) || side3 + side1 <= side2){
+    alert("Not a Triangle")
+  }
 }
