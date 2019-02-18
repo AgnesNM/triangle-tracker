4221e59747a65146651fa06f798eb308b1f221ba
Agnes NM <agnesndutamwaniki@gmail.com>
2019-02-18 09:21:25 +0300

    add media query to change div id sides border to green when the screen is less than 768 px

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ var sides = [];
     alert ("isosceles Triangle");
   } else if (side1 !== side2 && side2 !== side3 && side1 !== side3 && (side1 + side2 > side3 && side2 + side3 > side1) || side3 + side1 > side2) {
     alert ("Scalene Triangle");
-  } else if (side1 !== side2 && side2 !== side3 && side1 !== side3 && (side1 + side2 <= side3 || side2 + side3 <= side1) && side3 + side1 <= side2) {
+  } else if (side1 !== side2 && side2 !== side3 && side1 !== side3 && (side1 + side2 < side3 || side2 + side3 < side1) && side3 + side1 < side2) {
     alert ("Not a Triangle")
   }
 }
