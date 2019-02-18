9e6636346180d05c874945c4ae1b769caf6ad4d8
Agnes NM <agnesndutamwaniki@gmail.com>
2019-02-18 08:28:35 +0300

    edited not a triangle code

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function triangleSides () {
+function triangleSides() {
 var side1 = document.getElementById('side1').value;
 var side2 = document.getElementById('side2').value;
 var side3 = document.getElementById('side3').value;
@@ -11,7 +11,7 @@ var sides = [];
     alert ("isosceles Triangle");
   } else if (side1 !== side2 && side2 !== side3 && side3 !== side1 && side1 + side2 > side3 || side2 + side3 > side1 || side3 + side1 > side2) {
     alert ("Scalene Triangle");
-  } else if (side1 + side2 <= side3 || side2 + side3 <= side1 ||  side3 + side1 <= side2) {
+  } else if (side1 !== side2 && side2 !== side3 && side3 !== side1 && side1 + side2 <= side3 || side2 + side3 <= side1 ||  side3 + side1 <= side2) {
     alert("Not a Triangle");
   }
 }
